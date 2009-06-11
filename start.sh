bf9f0c9d0a4e7bff972bf9edd3cb20e28b046a6b
MEDIA <ruseel@sports-testweb2.daum.net>
2009-06-11 21:06:29 +0900

    add parameter +P 500000

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd `dirname $0`
-exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s mochiconntest
+exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s mochiconntest +P 500000
