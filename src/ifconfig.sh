958bd06ab68d4ec0ac5e135003168f57907f9b46
MEDIA <ruseel@sports-testweb1.daum.net>
2009-06-15 10:24:57 +0900

    add ifconfig setup for sports-testweb1

---
+++ src/ifconfig.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+/sbin/ifconfig eth0:1 10.99.99.11 $1
+/sbin/ifconfig eth0:2 10.99.99.12 $1
+/sbin/ifconfig eth0:3 10.99.99.13 $1
+/sbin/ifconfig eth0:4 10.99.99.3 $1
+/sbin/ifconfig eth0:5 10.99.99.4 $1
+/sbin/ifconfig eth0:6 10.99.99.5 $1
+/sbin/ifconfig eth0:7 10.99.99.6 $1
+/sbin/ifconfig eth0:8 10.99.99.7 $1
+/sbin/ifconfig eth0:9 10.99.99.8 $1
