017820c6829acc9cca1bca5e62b3edec981e3574
JEONG, MunShik <ruseel@daumcorp.com>
2009-07-06 12:25:52 +0900

    add more addresses for sports-livetest-channel2

--- src/httpclient.c
+++ src/httpclient.c
@@ -14,7 +14,7 @@
  
 #define BUFSIZE 4096
 #define NUMCONNS 62000
-#define SERVERADDR "10.99.99.24"
+#define SERVERADDR "10.99.99.121"
 #define SERVERPORT 8000
 #define SLEEP_MS 10
  
@@ -24,6 +24,26 @@ int bytes_recvd = 0;
 int chunks_recvd = 0;
 int closed = 0;
 int connected = 0;
+
+const char *addresses[] = {
+    "10.99.99.3",
+    "10.99.99.4",
+    "10.99.99.5",
+    "10.99.99.6",
+    "10.99.99.7",
+    "10.99.99.8",
+    "10.99.99.9",
+    "10.99.99.11",
+    "10.99.99.12",
+    "10.99.99.13",
+    "10.99.99.14",
+    "10.99.99.15",
+    "10.99.99.16",
+    "10.99.99.17",
+    "10.99.99.18",
+    "10.99.99.150",
+    "10.99.99.151"
+};
  
 // called per chunk received
 void chunkcb(struct evhttp_request * req, void * arg)
@@ -47,21 +67,19 @@ int main(int argc, char **argv)
     event_init();
     struct evhttp *evhttp_connection;
     struct evhttp_request *evhttp_request;
-    char addr[16];
     char path[32]; // eg: "/test/123"
-    int i,octet;
-    for(octet=3; octet<=8; octet++){
-        sprintf(&addr, "10.99.99.%d", octet);
+    int i,j,octet;
+    for(j=0; j<17; j++){
         for(i=1;i<=NUMCONNS;i++) {
             evhttp_connection = evhttp_connection_new(SERVERADDR, SERVERPORT);
-            evhttp_connection_set_local_address(evhttp_connection, (const char*)&addr);
+            evhttp_connection_set_local_address(evhttp_connection, (const char*)addresses[j]);
 	    evhttp_connection_set_local_port(evhttp_connection, (unsigned short)(1024+i));
             evhttp_set_timeout(evhttp_connection, 864000); // 10 day timeout
             evhttp_request = evhttp_request_new(reqcb, NULL);
             evhttp_request->chunk_cb = chunkcb;
             sprintf(&path, "/test/%d", ++connected);
             if(i%100==0)  
-		printf("Req: %s\t->\t%s\n", addr, &path);
+		printf("Req: %s\t->\t%s\n", addresses[j], &path);
             evhttp_make_request( evhttp_connection, evhttp_request, EVHTTP_REQ_GET, path );
             evhttp_connection_set_timeout(evhttp_request->evcon, 864000);
             event_loop( EVLOOP_NONBLOCK );
