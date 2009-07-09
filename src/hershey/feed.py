03858db064132cb45bd4f982c686f49f2f73e905
DaeEun Yu <moongtook@daumcorp.com>
2009-07-09 16:47:12 +0900

    update hershey

--- src/hershey/feed.py
+++ src/hershey/feed.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python2.6
 # vim: et sts=4 sw=4 ts=4 ai :
 import MySQLdb
+import MySQLdb.cursors
+
 from difflib import SequenceMatcher
 import time
 import datetime
 
-import router_client
+#import router_client
+import mock_router_client as router_client
 
 class ContextCursor:
     def __init__(self, cursor):
@@ -25,7 +28,9 @@ class SportsDatabase(object):
     def cursor(self):
         return ContextCursor(self.db.cursor())
     def close(self):
-        self.db.close()
+        if self.db:
+            self.db.close()
+            self.db=None
     def __enter__(self):
         return self
     def __exit__(self, type, value, traceback):
@@ -37,54 +42,103 @@ class SportsDatabase(object):
             r = c.fetchall()
         return r
 
-       
-if __name__=='__main__':
-    gameId ='20090701HHSK0'
-    sql = """
-        SELECT 
-            kbo.IE_LiveText.gameID    AS gameCode, 
-            kbo.IE_LiveText.LiveText  AS liveText, 
-            kbo.IE_LiveText.SeqNO     AS seqNo, 
-            kbo.IE_LiveText.Inning    AS inning, 
-            kbo.IE_LiveText.bTop      AS bTop, 
-            kbo.IE_LiveText.textStyle AS textStyle 
-        FROM 
-            kbo.IE_LiveText 
-        WHERE 
-            kbo.IE_LiveText.gameID = '%s'
-        ORDER BY 
-            seqNo 
-    """ % (gameId,)
+class RelayDatum:
+    def __init__(self):
+        self.before = {}
+        self.after = None
 
-    before = ()
-    #router_client.create(gameId)
-    print "created"
-    while True:
-        db = SportsDatabase(host='sports-livedb1', user='root', passwd='damman#2',db='kbo', charset='utf8')
-        with db as db:
-           after = db.execute(sql)
+    def sql(self):
+        raise NotImplemented()
 
-        cruncher = SequenceMatcher(None, before, after)
+    def list_of_dict_to_list_of_pairs(self, list_of_dict):
+        return tuple(tuple(d.items()) for d in list_of_dict)
+
+    def as_diff_strings(self):
+        cruncher = SequenceMatcher(None, self.list_of_dict_to_list_of_pairs(datum.before), self.list_of_dict_to_list_of_pairs(datum.after))
         for tag, i1, i2, j1, j2 in cruncher.get_opcodes():
             a = []
             if tag == "insert":
-                for t in after[j1:j2]:
-                    a.append("(%s) %s" % (t[2], t[1]))
-                router_client.send_as_raw(gameId, 
-                    ("<br/> [%s] [%s] " % (datetime.datetime.today(),tag)).join(a).encode("utf8"))
+                for t in datum.after[j1:j2]:
+                    yield tag, t
+                #yield "<br/> [%s] [%s] " % (datetime.datetime.today(),tag)).join(a).encode("utf8")
             elif tag == "delete":
-                for t in before[i1:i2]:
-                    a.append("(%s) %s" % (t[2], t[1]))
-                router_client.send_as_raw(gameId, 
-                    ("<br/> [%s] [%s] " % (datetime.datetime.today(),tag)).join(a).encode("utf8"))
+                for t in datum.before[i1:i2]:
+                    yield tag, t
+                #yield "<br/> [%s] [%s] " % (datetime.datetime.today(),tag)).join(a).encode("utf8")
             elif tag == "replace":
-                for t in after[j1:j2]:
-                    a.append("(%s) %s" % (t[2], t[1]))
-                router_client.send_as_raw(gameId, 
-                    ("<br/> [%s] [%s] " % (datetime.datetime.today(),tag)).join(a).encode("utf8"))
+                for t in datum.after[j1:j2]:
+                    yield tag, t
+                #yield "<br/> [%s] [%s] " % (datetime.datetime.today(),tag)).join(a).encode("utf8")
             elif tag == "equal":
-                print "equal"
+                yield "equal"
             else:
                 raise ValueError, 'unknown tag %r' % (tag,)
-        before = after
+
+class LiveText(RelayDatum):
+    def sql(self):
+        return """
+            SELECT 
+                kbo.IE_LiveText.gameID    AS gameCode, 
+                kbo.IE_LiveText.LiveText  AS liveText, 
+                kbo.IE_LiveText.SeqNO     AS seqNo, 
+                kbo.IE_LiveText.Inning    AS inning, 
+                kbo.IE_LiveText.bTop      AS bTop, 
+                kbo.IE_LiveText.textStyle AS textStyle 
+            FROM 
+                kbo.IE_LiveText 
+            WHERE 
+                kbo.IE_LiveText.gameID = '%s'
+            ORDER BY 
+                seqNo 
+        """
+
+class Score(RelayDatum):
+    def sql(self):
+        return  """
+        SELECT 
+            kbo.IE_Scoreinning.inning AS inning, 
+            kbo.IE_ScoreRHEB.Run as r, 
+            kbo.IE_ScoreRHEB.Hit as h, 
+            kbo.IE_ScoreRHEB.Error as e, 
+            kbo.IE_ScoreRHEB.BallFour as b, 
+            kbo.IE_Scoreinning.Score as score, 
+            kbo.IE_Scoreinning.bHome AS bhome 
+        FROM 
+            kbo.IE_Scoreinning 
+        INNER JOIN 
+            kbo.IE_ScoreRHEB 
+            ON 
+            (
+                kbo.IE_Scoreinning.gameID = kbo.IE_ScoreRHEB.gameID
+            ) 
+            AND 
+            (
+                kbo.IE_Scoreinning.bHome = kbo.IE_ScoreRHEB.bHome
+            ) 
+        WHERE 
+            kbo.IE_Scoreinning.gameID = '%s'
+        ORDER BY 
+            bhome DESC, 
+            inning  
+    """
+    
+       
+if __name__=='__main__':
+    gameId ='20090701HHSK0'
+    datums = [LiveText(), Score()]
+    #router_client.create(gameId)
+
+    print "created"
+    while True:
+        for datum in datums:
+            db = SportsDatabase(host='sports-livedb1', user='root', passwd='damman#2',db='kbo', charset='utf8', cursorclass=MySQLdb.cursors.DictCursor)
+            with db as db:
+               datum.after = db.execute(datum.sql() % gameId)
+
+            for tag, diff in datum.as_diff_strings():
+                router_client.send_as_raw(gameId, "%s, %s, %s" % (datum.__class__, tag, diff))
+
+            datum.before = datum.after
         time.sleep(5)
+
+
---
+++ src/hershey/mock_router_client.py
@@ -0,0 +1,4 @@
+import sys
+
+def send_as_raw(channel_id, msg):
+    print >> sys.stderr, msg
