f98bdedafffa17e7ce561eedbb96729b0852d704
MunShik JEONG <ruseel@daumcorp.com>
2009-07-13 17:35:12 +0900

    minor refactoring

--- src/hershey/router_client.py
+++ src/hershey/router_client.py
@@ -12,59 +12,31 @@ from meiji import RouterProxy
 
 def send_as_raw(channel, msg):
     try:
-        # Make socket
         transport = TSocket.TSocket('localhost', 9999)
-
-        # Buffering is critical. Raw sockets are very slow
         transport = TTransport.TBufferedTransport(transport)
-
-        # Wrap in a protocol
         protocol = TBinaryProtocol.TBinaryProtocol(transport)
-
-        # Create a client to use the protocol encoder
         router = RouterProxy.Client(protocol)
-
-        # Connect!
-        print 'transport.open()'
         transport.open()
 
         router.send_as_raw(channel, msg)
-        print 'send_as_raw()'
 
-        # Close!
         transport.close()
-
     except Thrift.TException, tx:
         print '%s' % (tx.message)
            
- 
 def create(channel):
     try:
-        # Make socket
         transport = TSocket.TSocket('localhost', 9999)
-
-        # Buffering is critical. Raw sockets are very slow
         transport = TTransport.TBufferedTransport(transport)
-
-        # Wrap in a protocol
         protocol = TBinaryProtocol.TBinaryProtocol(transport)
-
-        # Create a client to use the protocol encoder
         router = RouterProxy.Client(protocol)
-
-        # Connect!
-        print 'transport.open()'
         transport.open()
 
         router.create(channel)
-        print 'create'
 
-        # Close!
         transport.close()
-
     except Thrift.TException, tx:
         print '%s' % (tx.message)
-
 
 
 if __name__=="__main__":
