51a29edbf626b174e1f6ef7b76b674d99f900aa1
JEONG, MunShik <ruseel@daumcorp.com>
2009-07-15 11:27:28 +0900

    remove conversion from bitstring to 'list of bytes'

--- src/relay-thrift/router_stub.erl
+++ src/relay-thrift/router_stub.erl
@@ -1,30 +1,20 @@
 -module(router_stub).
 -export([start/0,stop/1,handle_function/2]).
--export([ping/1,create/1,destroy/1,send/2,send_as_raw/2]).
-
-ping(Msg) ->
-    Msg.
-to_string(BitStr) ->
-    case is_bitstring(BitStr) of
-        true ->
-            bitstring_to_list(BitStr);
-        _ ->
-            BitStr
-    end.
+-export([create/1,destroy/1,send/2,send_as_raw/2]).
 
 create(Channel) ->
-    router:create(to_string(Channel)).
+    router:create(Channel).
 
 destroy(Channel) ->
-    router:destroy(to_string(Channel)).
+    router:destroy(Channel).
 
 send(Channel,Msg) ->
-    router:send(to_string(Channel),Msg).
+    router:send(Channel,Msg).
 
 send_as_raw(Channel,Msg) ->
-    router:send_as_raw(to_string(Channel),Msg).
-%%
+    router:send_as_raw(Channel,Msg).
 
+%%
 
 start() ->
     start(9999).
@@ -35,12 +25,10 @@ start(Port) ->
                                 {service, routerProxy_thrift},   
                                 {port, Port},                   
                                 {name, routerProxy_server}]).                                                                                                        
-
 stop(Server) ->
     thrift_socket_server:stop(Server).                                                                                                                            
-
 handle_function(Function, Args) when is_atom(Function), is_tuple(Args) ->
-    case apply(?MODULE, Function, tuple_to_list(Args)) of                                                                                                         
+    case apply(?MODULE, Function, tuple_to_list(Args)) of
         ok -> ok;
         Reply -> {reply, Reply}
     end.
