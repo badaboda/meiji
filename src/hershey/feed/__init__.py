331dbef974b4346502d487f667f17fd7f22b2280
keebeom kim <badaboda@daumcorp.com>
2009-08-05 11:07:44 +0900

    add FeedTest class

--- src/hershey/feed/__init__.py
+++ src/hershey/feed/__init__.py
@@ -1,6 +1,8 @@
 # vim: et sts=4 sw=4 ts=4 ai fileencoding=utf8 :
 import MySQLdb
 import MySQLdb.cursors
+import unittest
+import merge
 
 import types, exceptions, sys, datetime, decimal
 from difflib import SequenceMatcher
@@ -270,3 +272,22 @@ def mypprint(o, write=write):
     else:
         raise ValueError(type(o))
 
+class FeedTest(unittest.TestCase):
+    def new_datum(self, klass, *args):
+        if args:
+            return klass(self.db, *args)
+        return klass(self.db, self.game_code)
+
+    def bootstrap_dict(self, klass, *args):
+        dict = self.new_datum(klass, *args).as_bootstrap_dict()
+        self.assertTrue(type(dict)==types.DictType)
+        return dict
+
+    def assertHierachy(self, path, hierachy_dict):
+        d=hierachy_dict
+        for k in path.split(':'):
+            self.assertTrue(d.has_key(unicode(k)), 'key(%s) not found in dict(%s)' % (k, repr(d)))
+            d=d[k]
+
+    def merge(self, dicts):
+        return reduce(lambda x,y: merge._merge_insert(x, y), dicts)
