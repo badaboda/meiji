1ab24e500795bba5fb548d6bac8f7e649afbc55a
MunShik JEONG <ruseel@daumcorp.com>
2009-08-12 15:08:27 +0900

    split DeltaGeneratorTest by RelayDatum type

--- src/hershey/delta_test.py
+++ src/hershey/delta_test.py
@@ -1,18 +1,30 @@
 # vim: fileencoding=utf-8 :
 import unittest, MySQLdb
+from difflib import SequenceMatcher
 
 import merge, feed, mock
 from feed import kbo
 
+class SequanceMatcherTest(unittest.TestCase):
+    def test_x(self):
+        s=SequenceMatcher(None, [(1, 1),(2, 2),(3, 3)], [(2, 2),(3, '3modified'),(4, 4)])
+        for o in s.get_opcodes():
+            print o
+    def test_y(self):
+        s=SequenceMatcher(None, [(1, 1)], [(1,1)])
+        for o in s.get_opcodes():
+            print o
 
-class DeltaGeneratorWithKeyedDatumTest(unittest.TestCase):
+class DeltaGeneratorTest(unittest.TestCase):
     def setUp(self):
         self.consumer = mock.MockJavascriptConsumer()
         self.delta=feed.DeltaGenerator(self.consumer)
-        self.KEYED_JSON_PATH="registry:players:**pcode:profile"
 
     def tearDown(self):
         pass
+
+class DeltaGeneratorWithKeyedDatumTest(DeltaGeneratorTest):
+    KEYED_JSON_PATH="registry:players:**pcode:profile"
 
     def test_insert_keyed_datum(self):
         self.delta.feed(mock.MockDatum(self.KEYED_JSON_PATH, [{ "pcode": "79260", "name": "장원삼" }]))
@@ -21,35 +33,6 @@ class DeltaGeneratorWithKeyedDatumTest(unittest.TestCase):
         self.assertEquals(
             ["db['registry']['players']['98260']['profile']={'pcode': '98260', 'name': %s};"%repr('정삼흠')],
             self.consumer.lst)
-
-    def test_insert_list_datum(self):
-        self.delta.feed(mock.MockDatumAsList("livetext", [{ "a": 1, "b": 2 }]))
-        self.delta.feed(mock.MockDatumAsList("livetext", [{ "a": 1, "b": 2 },
-                                                          { "a": 2, "b" : 2}]))
-        self.assertEquals(
-            ["db.livetext.push({'a': 2, 'b': 2});"], self.consumer.lst)
-
-    def test_mockdatum_as_atom(self):
-        self.delta.feed(mock.MockDatumAsAtom("scoreboard", [{"a": 1, "b": 2}]))
-        self.delta.feed(mock.MockDatumAsAtom("scoreboard", [{"a": 2, "b": 3}]))
-        self.assertEquals(
-            ["db['scoreboard']['a']=2;", "db['scoreboard']['b']=3;"], self.consumer.lst)
-
-    def test_waiting_batters(self):
-        self.delta.feed(mock.MockDatumAsList("waiting_batters", [{ "a": 1, "b": 1 },
-                                                          { "a": 2, "b": 2 },
-                                                          { "a": 3, "b": 3 }]))
-        self.delta.feed(mock.MockDatumAsList("waiting_batters", [{ "a": 2, "b": 2 },
-                                                          { "a": 3, "b": 3 },
-                                                          { "a": 4, "b": 4 }]))
-        self.assertEquals(
-            ["db.waiting_batters.splice(0, 1);", "db.waiting_batters.push({'a': 4, 'b': 4});"], self.consumer.lst)
-
-        #self.assertEquals(
-        #    ["db.waiting_batters=db.waiting_batters.slice(1)+{'a': 4, 'b': 4});", "db.waiting_batters.push({'a': 4, 'b': 4});"], self.consumer.lst)
-
-        #self.assertEquals(
-        #    ["db.waiting_batters=[{'a': 2, 'b': 2}, {'a': 3, 'b': 3}, {'a': 4, 'b': 4}]);"], self.consumer.lst)
 
     def test_delete(self):
         self.delta.feed(mock.MockDatum(self.KEYED_JSON_PATH, [{ "pcode": "79260", "name": "장원삼" },
@@ -67,6 +50,32 @@ class DeltaGeneratorWithKeyedDatumTest(unittest.TestCase):
         self.assertEquals(
             ["db['registry']['players']['79260']['profile']={'pcode': '79260', 'name': %s};"%repr('장원삼2')],
             self.consumer.lst)
+
+class DeltaGeneratorWithListDatum(DeltaGeneratorTest):
+    def test_insert_list_datum(self):
+        self.delta.feed(mock.MockDatumAsList("livetext", [{ "a": 1, "b": 2 }]))
+        self.delta.feed(mock.MockDatumAsList("livetext", [{ "a": 1, "b": 2 },
+                                                          { "a": 2, "b" : 2}]))
+        self.assertEquals(
+            ["db['livetext'].push({'a': 2, 'b': 2});"], self.consumer.lst)
+
+    def test_waiting_batters(self):
+        self.delta.feed(mock.MockDatumAsList("waiting_batters", [{ "a": 1, "b": 1 },
+                                                          { "a": 2, "b": 2 },
+                                                          { "a": 3, "b": 3 }]))
+        self.delta.feed(mock.MockDatumAsList("waiting_batters", [{ "a": 2, "b": 2 },
+                                                          { "a": 3, "b": 3 },
+                                                          { "a": 4, "b": 4 }]))
+        self.assertEquals(
+            ["db['waiting_batters'].splice(0, 1);", "db['waiting_batters'].push({'a': 4, 'b': 4});"], self.consumer.lst)
+
+class DeltaGeneratorWithAtomDatum(DeltaGeneratorTest):
+    def test_mockdatum_as_atom(self):
+        self.delta.feed(mock.MockDatumAsAtom("scoreboard", [{"a": 1, "b": 2}]))
+        self.delta.feed(mock.MockDatumAsAtom("scoreboard", [{"a": 2, "b": 3}]))
+        self.assertEquals(
+            ["db['scoreboard']['a']=2;", "db['scoreboard']['b']=3;"], self.consumer.lst)
+
 
 class DictMergeTest(unittest.TestCase):
     def test_merge_insert(self):
